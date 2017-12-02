8eb15c63de4fb4ec92dbe241120853b1ec36953b
Priyesh Tungare <priyeshtungare@Priyeshs-MacBook-Pro.local>
2017-12-02 13:04:04 +0800

    used onemap api as suggestion engine

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,50 @@ import typeahead from 'typeahead.js';
 import Bloodhound from 'bloodhound-js';
 
 $(() => {
-	let $title = $('.title').text();
-	console.log(`page title is ${$title}`);
+	
+	let $typeahead = $('.typeahead-location');
+	
+	let location = new Bloodhound({
+		datumTokenizer: function(datum) {
+			return Bloodhound.tokenizers.whitespace(datum.value);
+		},
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		remote: {
+			url: 'https://developers.onemap.sg/commonapi/search?searchVal=%QUERY&returnGeom=Y&getAddrDetails=Y',
+			wildcard: '%QUERY',
+			filter: (data) => {
+				return $.map(data.results, (result) => {
+					return {
+						value: result.SEARCHVAL,
+						lat: result.LATITUDE,
+						lon: result.LONGITUDE
+					};
+				});
+			}
+		}
+	});
+
+	location.initialize();
+
+	$typeahead.typeahead({
+		hint: false,
+		highlight: true,
+		minLength: 1
+	}, {
+		name: 'value',
+		displayKey: 'value',
+		source: location.ttAdapter()
+	});
+
+	$typeahead.on([
+		'typeahead:initialized',
+        'typeahead:initialized:err',
+        'typeahead:selected',
+        'typeahead:autocompleted',
+        'typeahead:cursorchanged',
+        'typeahead:opened',
+        'typeahead:closed'
+	].join(' '), (x) => {
+		console.log(`selected value is ${x}`);
+	});
 })
